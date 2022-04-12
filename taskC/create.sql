baf2daac33bdf72c79b0e8a2eaaf2f7162173b26
ganovak <gabriellanovak00@gmail.com>
2022-04-12 18:39:28 -0400

    add create.sql

--- taskC/create.sql
+++ taskC/create.sql
@@ -1,3 +1,4 @@
+DROP DATABASE ridekick; 
 CREATE DATABASE ridekick;
 USE ridekick;
 
@@ -9,6 +10,19 @@ CREATE TABLE passenger (
     PRIMARY KEY (pid)
 );
 
+CREATE TABLE service (
+	sid INT,
+    startL VARCHAR(20) NOT NULL,
+    endL VARCHAR(20) NOT NULL,
+    isactive BOOLEAN NOT NULL DEFAULT false,
+    napp INT NOT NULL DEFAULT 0,
+    nseats INT NOT NULL,
+    deptime TIME NOT NULL,
+    date DATE NOT NULL,
+    PRIMARY KEY (sid),
+    CHECK (napp <= nseats)
+);
+
 CREATE TABLE appointment (
 	date DATE NOT NULL,
 	time TIME NOT NULL,
@@ -18,18 +32,6 @@ CREATE TABLE appointment (
     PRIMARY KEY (aid),
     FOREIGN KEY (isfor_sid) REFERENCES service(sid),
     FOREIGN KEY (schedules_pid) REFERENCES passenger(pid)
-);
-
-CREATE TABLE service (
-	sid INT,
-    startL VARCHAR(20) NOT NULL,
-    endL VARCHAR(20) NOT NULL,
-    isactive BOOLEAN NOT NULL DEFAULT false,
-    napp INT NOT NULL DEFAULT 0 CHECK (napp <= nseats),
-    nseats INT NOT NULL,
-    deptime TIME NOT NULL,
-    date DATE NOT NULL,
-    PRIMARY KEY (sid)
 );
 
 CREATE TABLE delay (
